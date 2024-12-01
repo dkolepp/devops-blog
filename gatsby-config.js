c18b077ee46623cda2d53fe8b8c73d9fdb4e717c
Dan Kolepp <dan@kolepp.dev>
2024-11-30 19:26:44 -0500

    finished step 4 of tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `DevOps Blog`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
@@ -22,6 +22,13 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
